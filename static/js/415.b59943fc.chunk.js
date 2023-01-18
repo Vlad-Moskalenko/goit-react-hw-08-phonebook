c51676ca21d4b52dec7646c27044edf043bf350b
Vlad-Moskalenko <94741971+Vlad-Moskalenko@users.noreply.github.com>
2023-01-18 11:33:48 +0000

    Deploying to gh-pages from @ Vlad-Moskalenko/goit-react-hw-08-phonebook@665bb0b8c5b4788e6a9339152de88e13791a13f0 🚀

---
+++ static/js/415.b59943fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_07_phonebook=self.webpackChunkgoit_react_hw_07_phonebook||[]).push([[415],{415:function(e,t,n){n.r(t);var i=n(329),o={title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,i.jsx)(i.Fragment,{children:(0,i.jsxs)("h1",{style:o.title,children:["Contacts manager welcome page"," ",(0,i.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=415.b59943fc.chunk.js.map
